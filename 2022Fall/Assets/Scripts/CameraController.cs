d455d87040f325a73f311ab17b769e17c140ab22
DESKTOP-BHNU3C3\Merry <mmhospe@yahoo.com>
2022-09-22 15:52:34 -0400

    More script updates

--- 2022Fall/Assets/Scripts/CameraController.cs
+++ 2022Fall/Assets/Scripts/CameraController.cs
@@ -2,39 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//!! If your camera is stuttering while trying to follow the target, go to the target's rigidbody and change the interpolate value from none to interpolate
 public class CameraController : MonoBehaviour
 {
-    public Transform target;
-    public float smoothSpeed = 0.125f;
-    public Vector3 offset;
+    public Transform target; //whatever our camera tracks. This can be changed by changing what transform this is linked to
+    public float smoothSpeed = 0.125f; //how much our camera lags behind the target
+    public Vector3 offset; //even in a 2D game, our camera technically exists in 3D space, so be sure to use Vector3's
 
-    private Vector3 _velocity = Vector3.zero;
-    private PlayerController _player;
+    private Vector3 _velocity = Vector3.zero; //Vector3.zero essentially initializes a Vector3 as (0,0,0)
 
-    public float limitLeft, limitRight, limitBottom, limitTop;
+    public float limitLeft, limitRight, limitBottom, limitTop; //these are as far as the camera can go in each direction in Unity units
     
-    // Start is called before the first frame update
     void Start()
     {
-        if(target == null)
+        if(target == null) //if we never assigned a target in our inspector in Unity, find the player
         {
-            target = GameObject.FindGameObjectWithTag("Player").transform;
+            target = GameObject.FindGameObjectWithTag("Player").transform; 
         }
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        Transform currentTarget = target;
-        Vector3 desiredPosition = currentTarget.position + offset;
+        Transform currentTarget = target; //at the start of our LateUpdate, the camera will try to move to our target
+        Vector3 desiredPosition = currentTarget.position + offset; //we calculate a desired offset to this movement utilizing the offset we set above
 
-        Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref _velocity, smoothSpeed);
+        Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref _velocity, smoothSpeed); //we use smoothdamp instead of lerp because our target is likely moving (lerp can get jittery) and it, over time, calculates a smooth movement from our current to our desired position
 
-        //boundreis
-        transform.position = new Vector3(Mathf.Clamp(smoothedPosition.x, limitLeft, limitRight), Mathf.Clamp(smoothedPosition.y, limitBottom, limitTop), smoothedPosition.z); 
+
+        //boundaries 
+        transform.position = new Vector3(Mathf.Clamp(smoothedPosition.x, limitLeft, limitRight), Mathf.Clamp(smoothedPosition.y, limitBottom, limitTop), smoothedPosition.z); //we now adjust our calculation to make sure we don't move out of the boundaries we've set. IE, if our calculated smoothPosition is 11 on the x axis, but we have a rightLimit of 10, it will automatically bring 11 down to 10
     }
 
-    public void ChangeTarget(GameObject newTarget)
+    public void ChangeTarget(GameObject newTarget) //we make this public so that other scripts can feed in new game objects for our camera to track
     {
         target = newTarget.transform;
     }
--- 2022Fall/Assets/Scripts/DEMOS/RandomizationDemo.cs
+++ 2022Fall/Assets/Scripts/DEMOS/RandomizationDemo.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class RandomizationDemo : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     void Start()
     {
-        
+        //much of your randomization will utilizae Random.Range. The first number in the () is the min, and the second value is the max. From there, it will generate a number between those values
+
+        float randomFloat = Random.Range(-5.0f, 5.0f); // randomize between -5, 5, but with 1 decimal place
+        int randomInt = Random.Range(-5, 5); //randomize between -5, 5, not decimal places
+
+        //REMINDER: the max is very rarely randomized. If you need to choose a number from 1-10, you'll need to randomize 1,11, and then have an edge case test for the very unlikely case that it lands on 11 
+        //this is because Random.Range spits out a float, even when you are assigning it to an int. So. For every number it will have decimal places that cause it to round down to that number, as per how integers work. Therefore, the final number will never have decimal places and any varation of that number will neer come up
+        //Basically: 1.1, 1.45, 1.99 -> will all round down to 1. But the upper limit of 11 can ONLY generate 11.0, making it almost impossibly small to hit
+
+        //here I'll add my test
+        bool valid = false;
+
+        while (valid == false)
+        {
+            int randomInt2 = Random.Range(1, 11);
+            
+            if(randomInt2 >= 11) //if it managed to roll 11, make sure wedon't escape the loop and make it generate a new number
+                valid = false;
+            else
+                valid = true;
+        }
+
+        //often you won't need this test, but if you need to randomly choose an index without an out of index error, you'll wanna add something like this in
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
