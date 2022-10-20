f532edcb8412f7a728adca76641c24ec8287cfb7
MHOSS <mmhospe@yahoo.com>
2022-10-20 12:37:31 -0400

    Added demo characters 10/20

--- 2022Fall/Assets/Scripts/DEMOS/Outside Files/SavingData_DEMO.cs
+++ 2022Fall/Assets/Scripts/DEMOS/Outside Files/SavingData_DEMO.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using TMPro;
+
 [System.Serializable] //include this at the top to make it savable as a file
 public class SavingData_DEMO //we do not use : MonoBehaviour because this will not be a component on an object
 {
@@ -10,7 +12,6 @@ public class SavingData_DEMO //we do not use : MonoBehaviour because this will n
     public int playerLevel;
     public float[] playerPosition; //here we are planning to save a Vector3, but because Vector3 isn't really a data type outside of Unity, we need to convert it to something else
     
-
     //Here we need to make a way our script can get the data from the objects we want to save, such as the player
     public SavingData_DEMO(SavingDataPlayer_DEMO player) //this is a Constructor. A Constructor for a class is essentially a setup function otherscripts can call
     {
